c3f3ff978c46807194578b196ac9381073580ef6
0xlax <harshu8310@gmail.com>
2022-01-28 09:59:47 +0530

    update

---
+++ src/main.rs
@@ -0,0 +1,45 @@
+struct Node {
+    children: Vec<Node>,
+
+    node_type: NodeType,
+}
+
+enum NodeType {
+    Text(String),
+    Element(ElementData),
+}
+
+struct ElementData {
+    tag_name: String,
+    attributes: AttrMap,
+}
+
+type AttrMap = HashMap<String, String>;
+
+fn text(data: String) -> Node {
+    Node { children: Vec::new(), node_type: NodeType::Text(data) }
+}
+
+fn elem(name: String, attrs: AttrMap, children: Vec<Node>) -> Node {
+    Node {
+        children: children,
+        node_type: NodeType::Element(ElementData {
+            tag_name: name,
+            attributes: attrs,
+        })
+    }
+
+}
+
+impl ElementData {
+    pub fn id(&self) -> Option<&String> {
+        self.attributes.get("id")
+    }
+
+    pub fn classes(&self) -> HashSet<&str> {
+        match self.attributes.get("class") {
+            Some(classlist) => classlist.split(' ').collect(),
+            None => HashSet::new()
+        }
+    }
+}
